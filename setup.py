1bf466830001296c2e5ddd3843126bccc7f86bb5
onagorodniuk <oleksandr.nagorodniuk@onseostudio.com>
2016-07-13 16:32:55 +0300

    1st

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='cabot-alert-ses',
+      version='1.0.0',
+      description='SES plugin for cabot',
+      author='onagorodniuk',
+      author_email='info@arachnys.com',
+      url='http://cabotapp.com',
+      packages=find_packages(),
+      download_url = 'https://github.com/onagorodniuk/cabot-alert-ses/tarball/1.0.0',
+     )
